95eb285e88c0076c3e0f0bdd49423d5514ab6eb1
boyzcodez <tommi.korh954@gmail.com>
2026-01-23 17:57:05 +0200

    Lisää tehtäviä done

---
+++ src/viikko1/Puoluetuki.java
@@ -0,0 +1,18 @@
+package viikko1;
+
+import java.util.Scanner;
+
+public class Puoluetuki {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int support = 148175;
+
+        System.out.print("Anna kansanedustajien lukumäärä: ");
+        int amount = scanner.nextInt();
+
+        amount = amount * support;
+
+        System.out.println("Puoluetuki on " + amount +" euroa");
+        scanner.close();
+    }
+}
