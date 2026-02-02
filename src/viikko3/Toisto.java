028fc5e688d54198e21f2657e227997dfe0d7e8a
boyzcodez <tommi.korh954@gmail.com>
2026-02-02 21:35:19 +0200

    more

---
+++ src/viikko3/Toisto.java
@@ -0,0 +1,9 @@
+package viikko3;
+
+public class Toisto {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
