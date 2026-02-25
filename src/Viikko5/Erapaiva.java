241e64f2e22c6417018d8c0bcf5c226ef4835c65
boyzcodez <tommi.korh954@gmail.com>
2026-02-25 18:39:16 +0200

    päivä / kalenteri harjotukset

---
+++ src/Viikko5/Erapaiva.java
@@ -0,0 +1,25 @@
+package Viikko5;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Erapaiva {
+    public static void main(String[] args) {
+
+        final int EXPIRATION = 14;
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-M-d");
+
+        System.out.print("Anna laskun päivämäärä (vvvv-kk-pp): ");
+        String ans = scanner.nextLine();
+
+        LocalDate date = LocalDate.parse(ans, dtf);
+        date = date.plusDays(EXPIRATION);
+
+        System.out.println("Eräpäivä on " + date);
+
+        scanner.close();
+    }
+}
---
+++ src/Viikko5/Kalenteri.java
@@ -0,0 +1,48 @@
+package Viikko5;
+
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.Scanner;
+
+public class Kalenteri {
+    public static void main(String[] args) {
+
+        final String WEEK = "Ma Ti Ke To Pe La Su";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Syötä vuosi: ");
+        int year = scanner.nextInt();
+
+        System.out.print("Syötä kuukausi: ");
+        int month = scanner.nextInt();
+        scanner.close();
+
+        LocalDate firstDay = LocalDate.of(year, month, 1);
+        int startColumn = firstDay.getDayOfWeek().getValue(); // 1=Mon ... 7=Sun
+
+        YearMonth ym = YearMonth.of(year, month);
+        int daysInMonth = ym.lengthOfMonth();
+
+        System.out.println();
+
+        System.out.println(WEEK);
+
+        // Print leading spaces
+        for (int i = 1; i < startColumn; i++) {
+            System.out.print("   ");
+        }
+
+        int column = startColumn;
+
+        // Print days
+        for (int day = 1; day <= daysInMonth; day++) {
+            System.out.printf("%-3d", day);
+
+            if (column % 7 == 0) {
+                System.out.println();
+            }
+            column++;
+        }
+    }
+}
