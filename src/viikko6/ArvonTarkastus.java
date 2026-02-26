343a79d88326b66921cd285908ce05e5d780405c
boyzcodez <tommi.korh954@gmail.com>
2026-02-26 16:03:54 +0200

    error handling juttuja

---
+++ src/viikko6/ArvonTarkastus.java
@@ -0,0 +1,20 @@
+package viikko6;
+
+import java.util.Scanner;
+
+public class ArvonTarkastus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Syötä luku väliltä 0-23: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        if (!(num >= 0 && num <= 23)){
+            throw new IllegalArgumentException();
+        }
+        else {
+            System.out.println("Luku " + num + " on sallittu.");
+        }
+    }
+}
---
+++ src/viikko6/KysyUudestaan.java
@@ -0,0 +1,24 @@
+package viikko6;
+
+import java.util.Scanner;
+
+public class KysyUudestaan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Syötä kokonaisluku: ");
+
+        try {
+            int num = scanner.nextInt();
+
+            System.out.println("Syötit luvun " + num + ".");
+            
+        } catch (Exception e) {
+            System.out.println("Virheellinen luku!\n");
+
+            main(args);
+        }
+
+        scanner.close();
+    }
+}
